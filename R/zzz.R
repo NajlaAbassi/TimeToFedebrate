44344c45262f59ed86d1fcd585106ffb018de389
Najla Abassi <abassi.nejla96@gmail.com>
2025-09-04 16:23:00 +0200

    use pkgStartupMsg instead of msg

--- R/zzz.R
+++ R/zzz.R
@@ -4,5 +4,5 @@
   random_int <- sample(1:max_reasons, 1)
   random_reason <- we_depend_on_you(random_int)
   # display the random reason
-  message("We depend on you, Fede, ",random_reason)
+  packageStartupMessage("We depend on you, Fede, ",random_reason)
 }
